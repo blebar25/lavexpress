0a7ee658fbbba583930d185f60e8d2758e0d426c
blebar25-lab <56796185+blebar25-lab@users.noreply.github.com>
2025-01-16 18:59:39 +0200

    fix: ajout de la configuration des icônes Leaflet pour les marqueurs de la carte

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,19 @@ import {
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
+import icon from 'leaflet/dist/images/marker-icon.png'
+import iconShadow from 'leaflet/dist/images/marker-shadow.png'
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 interface Location {
   name: string

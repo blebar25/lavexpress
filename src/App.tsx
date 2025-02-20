795e1aee50e6a4101b3d1a8d92d6996fa9f4778c
blebar25 <56796185+blebar25-lab@users.noreply.github.com>
2025-02-20 18:03:49 +0200

    Move Pinel images to assets and fix image paths

--- src/App.tsx
+++ src/App.tsx
@@ -760,7 +760,7 @@ export default function App() {
                     transition={{ duration: 0.8, ease: "easeInOut" }}
                   >
                     <svg className="w-full h-full" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M12 3L1 9L5 11.18V17.18L12 21L19 17.18V11.18L21 10.09V17H23V9L12 3ZM18.82 9L12 12.72L5.18 9L12 5.28L18.82 9ZM17 15.99L12 18.72L7 15.99V12.27L12 15L17 12.27V15.99Z"/>
+                    <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
                     </svg>
                   </motion.div>
                   <h3 className="text-2xl font-semibold text-white mb-4">Formation complète</h3>
